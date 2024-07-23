24565b3b11c13d7272e93cab84d244e70ed584bb
kamilla3t <kamilla3t@gmail.com>
2024-07-22 23:33:14 -0300

    meu primieiro crud usuarios

--- Models/Projeto.cs
+++ Models/Projeto.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 namespace Exo.WebApi.Models
 {
     public class Projeto
+
     {
         public int Id { get; set; }
         public string NomeDoProjeto { get; set; }
